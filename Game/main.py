77fb3d16c6717a16240a8351dd3e661ffa119f3c
BrendonOS <32614296+BrendonOS@users.noreply.github.com>
2018-06-19 12:37:49 -0400

    Create main.py

---
+++ Game/main.py
@@ -0,0 +1,7 @@
+def episodeselect():
+     print ("What episode would you like to play? NOTE: IT WILL NOT WORK IF YOU SKIP EPISODES! Type JUST THE NUMBER of the episode you would like to play.")
+     selectedepisode = input()
+     if selectedepisode = ("1"):
+          import ep1 from episodes
+     if selectedisode = ("2"):
+          
