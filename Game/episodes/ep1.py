352bd29bee2178f0cdd48fce44e8626dffcddf21
BrendonOS <32614296+BrendonOS@users.noreply.github.com>
2018-06-20 11:40:24 -0400

    Update ep1.py

--- Game/episodes/ep1.py
+++ Game/episodes/ep1.py
@@ -6,6 +6,13 @@ def play():
      playerroot.data_setdefault()
      story = ("started")
      ep1_progress = ("started")
-     print ("You walk into the house of your teacher,
+     print ("Hundreds of years ago, the world was fighting in a large war against the gods. Then, one day, out of nowhere.... the world lost to the gods.")
+     print ("However, this caused the world to be reborn as a new world, and the gods, luckily, still haven't taken it.")
+     playername = input ("Tell me, hero.... what is your name?")
+     gender = input ("Ah, yes, a wonderful name. But, might I ask, what is your gender?")
+         
+     
+         
+     
      
      
