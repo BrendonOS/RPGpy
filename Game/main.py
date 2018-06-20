bae07e2265217f301f08637f109e8f4e693e2050
BrendonOS <32614296+BrendonOS@users.noreply.github.com>
2018-06-20 08:11:47 -0400

    Update main.py

--- Game/main.py
+++ Game/main.py
@@ -2,11 +2,11 @@ def episodeselect():
      print ("What episode would you like to play? NOTE: IT WILL NOT WORK IF YOU SKIP EPISODES! Type JUST THE NUMBER of the episode you would like to play.")
      selectedepisode = input()
      if selectedepisode = ("1"):
-          import ep1 from episodes
+          from episodes import ep1
      if selectedepisode = ("2"):
-          import ep2 from episodes
+          from episodes import ep2
      if selectedepisode = ("3"):
-          import ep3 from episodes
+          from episodes import ep3
 def episodereference():
      
           
