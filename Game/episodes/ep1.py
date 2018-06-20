e8c05e2427b811af667f3528e808b014f5581a98
BrendonOS <32614296+BrendonOS@users.noreply.github.com>
2018-06-20 10:14:42 -0400

    Update ep1.py

--- Game/episodes/ep1.py
+++ Game/episodes/ep1.py
@@ -1,6 +1,11 @@
 from characters import narrator
 from player import playerroot
+from Game import functions
+
 def play():
+     playerroot.data_setdefault()
      story = ("started")
      ep1_progress = ("started")
+     print ("You walk into the house of your teacher,
+     
      
