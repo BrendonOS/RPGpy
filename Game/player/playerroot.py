4891fdd8e60ca1d1a96f76298d33ca517c95d570
BrendonOS <32614296+BrendonOS@users.noreply.github.com>
2018-06-20 10:10:31 -0400

    Update playerroot.py

--- Game/player/playerroot.py
+++ Game/player/playerroot.py
@@ -2,5 +2,7 @@ def data_setdefault():
   chartype = ("player")
   playerrelationshipsallowed = ("true")
   playerinteractive = ("$MAINPLAYER$CONTROLLABLE$")
+  gender = ("undefined$null")
+  name = ("undefined$null")
   
   
