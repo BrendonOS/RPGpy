a2e054843b0b22aab6092a44cad2315f3f36d973
BrendonOS <32614296+BrendonOS@users.noreply.github.com>
2018-06-21 14:21:33 -0400

    Update functions.py

--- Game/functions.py
+++ Game/functions.py
@@ -3,3 +3,7 @@ def speak():
 def resetepisode():
      if selectedepisode = ("1"):
           ep1_progress = ("not-started")
+def shop():
+     from functions import shop
+     shop.data()
+     shop.main()
