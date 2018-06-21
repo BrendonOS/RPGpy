f3de7a7a9e4a9fba54a1aa9854a5f85f6a9c5034
BrendonOS <32614296+BrendonOS@users.noreply.github.com>
2018-06-21 14:42:29 -0400

    Update coins.py

--- Game/items/coins.py
+++ Game/items/coins.py
@@ -1,4 +1,7 @@
+from Game import functions
 def coin():
      itemtype = ("$currency$")
      usable = ("false")
      usableasweapon = ("false")
+     functions.shop()
+     currency = itemnumbercoins
