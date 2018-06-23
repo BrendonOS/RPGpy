cd56bcdc7415bd572229abf548cca72b3c86f90d
BrendonOS <32614296+BrendonOS@users.noreply.github.com>
2018-06-23 17:18:02 -0400

    Update inventory.py

--- Game/function/inventory.py
+++ Game/function/inventory.py
@@ -1,1 +1,17 @@
+def checkforitem():
+  if itembeingcheckedforexists = ("exists-in-inventory"):
+       hasitem = ("true")
+  if itembeingcheckedforexists = ("does-not-exist-in-inventory"):
+       hasitem = ("false")
+def viewallitems():
+  print ("Here are the available items:")
+  print (itemslot1)
+  print (itemslot2)
+  print (itemslot3)
+  print (itemslot5)
+  print (itemslot6)
+  print (itemslot7)
+  print (itemslot8)
+  
 
+  
