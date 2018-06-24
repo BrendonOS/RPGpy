6538504ab12d6726c55df1861fb44b30af188ccc
Ashish Karel <ashishbkarel@gmail.com>
2018-06-24 21:45:31 +0530

    Update main.py
    Updated the if condition equations!

--- Game/main.py
+++ Game/main.py
@@ -1,12 +1,12 @@
 def episodeselect():
      print ("What episode would you like to play? NOTE: IT WILL NOT WORK IF YOU SKIP EPISODES! Type JUST THE NUMBER of the episode you would like to play.")
      selectedepisode = input()
-     if selectedepisode = ("1"):
+     if selectedepisode == ("1"):
           from episodes import ep1
           ep1.play()
-     if selectedepisode = ("2"):
+     if selectedepisode == ("2"):
           from episodes import ep2
-     if selectedepisode = ("3"):
+     if selectedepisode == ("3"):
           from episodes import ep3
 def episodereference():
      
